8ecd1a6a58bc0ef39818f1b3b4fd968dcdc82f2f
hamza-ihind <ihind.mizo@gmail.com>
2022-08-10 16:40:29 +0100

    changes

---
+++ stage/client/src/components/Ajout-prof/ajout-prof.component.jsx
@@ -0,0 +1,67 @@
+import { useState, useEffect } from 'react'
+import './ajout-prof.styles.scss'
+import Axios from 'axios'
+
+
+const AjoutProf = () => {
+
+    // states des informations des profs
+    const [nom, setNom] = useState("")
+    const [matricule, setMatricule] = useState("")
+    const [email, setEmail] = useState("")
+
+    const [profs, setProfs] = useState([])
+
+    useEffect(() => {
+        Axios.get("http://localhost:3001/api/get").then((response) => {
+            setProfs(response.data)
+        })
+    }, [])
+
+    const createProf = () => {
+        Axios.post("http://localhost:3001/api/insert", { nom, matricule, email })
+        setProfs([...profs, { nom, matricule, email }])
+    }// the UI
+    return (
+        <div>
+            <h1 className='title'>Ajoutez un prof</h1>
+
+            <div className='add-prof'>
+                <input type="text" placeholder='Nom' onChange={(event) => { setNom(event.target.value) }} />
+                <input type="text" placeholder='Matricule' onChange={(event) => { setMatricule(event.target.value) }} />
+                <input type="text" placeholder='Email' onChange={(event) => { setEmail(event.target.value) }} />
+                <button onClick={createProf}>Ajouter</button>
+            </div>
+
+            <table className='container'>
+                <thead>
+                    <tr>
+                        <th>Nom</th>
+                        <th>Matricule</th>
+                        <th>Email</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {profs.map((prof) => {
+                        return (
+                            <tr className='elems-container'>
+                                <td className='elem'>{prof.nom}</td>
+                                <td className='elem'>{prof.matricule}</td>
+                                <td className='elem'>{prof.email}</td>
+                                <td className='list-buttons'>
+                                    <button>Edit</button>
+                                    <button>Delete</button>
+                                </td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+
+
+export default AjoutProf;
--- stage/server/index.js
+++ stage/server/index.js
@@ -1,53 +1,53 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const cors = require('cors');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
 
-//
-
-const db = mysql.createConnection({
-	user: 'E11even',
+const db = mysql.createPool({
+	user: 'root',
 	host: 'localhost',
-	password: 'Algohamza2003',
+	password: 'root123',
 	database: 'timetable_db',
 });
 
-//
+// Ajouter des profs: Par Admin
 
-app.post('/create', (req, res) => {
+// Ajouter
+
+app.post('/api/insert', (req, res) => {
 	const matricule = req.body.matricule;
 	const nom = req.body.nom;
 	const email = req.body.email;
+	const sqlInsert = 'INSERT INTO profs (matricule, nom, email) VALUES (?,?,?)';
 
-	db.query(
-		'INSERT INTO profs (matricule, nom, email) VALUES (?,?,?)',
-		[matricule, nom, email],
-		(err, result) => {
-			if (err) {
-				console.error(err + ' il faut que tu fixe ton problème');
-			} else {
-				res.send(result);
-			}
-		},
-	);
-});
-
-//
-
-app.get('/profs', (req, res) => {
-	db.query('SELECT * FROM profs', (err, result) => {
-		if (err) {
-			console.log(err);
-		} else {
-			res.send(result);
-		}
+	db.query(sqlInsert, [matricule, nom, email], (err, result) => {
+		console.log(result);
 	});
 });
 
-app.listen(3001, () => {
-	console.log('working');
+// Afficher
+
+app.get('/api/get', (req, res) => {
+	const sqlSelect = 'SELECT * FROM profs';
+	db.query(sqlSelect, (err, result) => {
+		res.send(result);
+	});
 });
+
+// Editer
+
+// Supprimer
+
+app.listen(3001, () => {
+	console.log('worked back');
+});
+
+// /////////////
+
+// Ajouter des filières: Par Admin
